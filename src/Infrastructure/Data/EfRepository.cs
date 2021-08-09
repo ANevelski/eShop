2d1dc55331cea66736b263e5f53aad624789d0c6
ANevelski <Aliaksei_Nevelski@epam.com>
2021-08-09 22:53:48 +0200

    Add project files.

---
+++ src/Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,122 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data
+{
+    public class OrderV2
+    {
+        public OrderV2()
+		{
+            Items = new List<OrderItem>();
+        }
+        public Address ShippingAddress { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public decimal FinalPrice { get; set; }
+    }
+
+    /// <summary>
+    /// "There's some repetition here - couldn't we have some the sync methods call the async?"
+    /// https://blogs.msdn.microsoft.com/pfxteam/2012/04/13/should-i-expose-synchronous-wrappers-for-asynchronous-methods/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EfRepository<T> : IAsyncRepository<T> where T : BaseEntity, IAggregateRoot
+    {
+        protected readonly CatalogContext _dbContext;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public EfRepository(CatalogContext dbContext, IHttpClientFactory clientFactory)
+        {
+            _dbContext = dbContext;
+            _clientFactory = clientFactory;
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var keyValues = new object[] { id };
+            return await _dbContext.Set<T>().FindAsync(keyValues, cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec, CancellationToken cancellationToken = default)
+        {
+            var specificationResult = ApplySpecification(spec);
+            return await specificationResult.ToListAsync(cancellationToken);
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec, CancellationToken cancellationToken = default)
+        {
+            var specificationResult = ApplySpecification(spec);
+            return await specificationResult.CountAsync(cancellationToken);
+        }
+
+        public async Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            if (entity is Order)
+            {
+                var orderEntity = entity as Order;
+                var orderV2 = new OrderV2();
+                orderV2.ShippingAddress = orderEntity.ShipToAddress;
+                orderV2.Items.AddRange(orderEntity.OrderItems);
+                orderV2.FinalPrice = orderEntity.Total();
+
+                var todoItemJson = new StringContent(JsonSerializer.Serialize(orderV2), Encoding.UTF8, "application/json");
+
+                var httpClient = _clientFactory.CreateClient();
+                using var httpResponse = await httpClient.PostAsync("https://orderdetailssaver.azurewebsites.net/api/DeliveryOrderProcessor", todoItemJson, cancellationToken);
+
+            }
+            else
+            {
+                await _dbContext.Set<T>().AddAsync(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+            }
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<T> FirstAsync(ISpecification<T> spec, CancellationToken cancellationToken = default)
+        {
+            var specificationResult = ApplySpecification(spec);
+            return await specificationResult.FirstAsync(cancellationToken);
+        }
+
+        public async Task<T> FirstOrDefaultAsync(ISpecification<T> spec, CancellationToken cancellationToken = default)
+        {
+            var specificationResult = ApplySpecification(spec);
+            return await specificationResult.FirstOrDefaultAsync(cancellationToken);
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            var evaluator = new SpecificationEvaluator<T>();
+            return evaluator.GetQuery(_dbContext.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
